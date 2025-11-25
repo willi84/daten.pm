7086bbc87f1ad298460a34a1bef3a0a00830c669
Robert Willemelis <robert.dev@gmx.de>
2025-11-25 12:21:22 +0100

    feat: add different reverse api

--- src/backend/apps/api/endpoints/geo/geo.config.ts
+++ src/backend/apps/api/endpoints/geo/geo.config.ts
@@ -1,2 +1,6 @@
 export const REVERSE_GEOCODE_API =
-    'https://nominatim.politik.de/nominatim/reverse?format=json&lat={LAT}&lon={LON}&addressdetails=1';
+    'https://nominatim.openstreetmap.org/reverse?format=json&lat={LAT}&lon={LON}&addressdetails=1';
+    // 'https://nominatim.politik.de/nominatim/reverse?format=json&lat={LAT}&lon={LON}&addressdetails=1';
+
+
+    // https://nominatim.openstreetmap.org/reverse?lat=<value>&lon=<value>&<params>
